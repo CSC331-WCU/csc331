3fa6570dbe244516cadb3a1bb243a5a097f28fdc
Linh B. Ngo <lngo@wcupa.edu>
2023-08-27 09:38:26 -0400

    update

--- site.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -x
-
-bundle exec jekyll build
-scp -rq _site/* lngo@cs.wcupa.edu:~/public_html/csc331/
-ssh lngo@cs.wcupa.edu "bash /home/lngo/keys/keys.sh /home/lngo/public_html/csc331/"
